162ac26c4accd52b5d27843e60838fb856a6bed1
Optega <optega@gmail.com>
2024-11-10 16:55:35 +0200

    fix: update icon source paths to include '/images/icons/' for consistency

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,61 +2,61 @@ export const categoryMenuList = [
   {
     id: 1,
     title: "Телефони",
-    src: "/smart phone icon.png",
+    src: "/images/icons/smart phone icon.png",
     href: "/shop/smart-phones",
   },
   {
     id: 2,
     title: "Планшети",
-    src: "/tablet icon.png",
+    src: "/images/icons/tablet icon.png",
     href: "/shop/tablets",
   },
   {
     id: 3,
     title: "Мишки",
-    src: "/mouse icon.png",
+    src: "/images/icons/mouse icon.png",
     href: "/shop/mouses",
   },
   {
     id: 4,
     title: "Камери",
-    src: "/camera icon.png",
+    src: "/images/icons/camera icon.png",
     href: "/shop/cameras",
   },
   {
     id: 5,
     title: "Годинники",
-    src: "/smart watch.png",
+    src: "/images/icons/smart watch.png",
     href: "/shop/watches",
   },
   {
     id: 6,
     title: "Ноутбуки",
-    src: "/laptop icon.png",
+    src: "/images/icons/laptop icon.png",
     href: "/shop/laptops",
   },
   {
     id: 7,
     title: "ПК",
-    src: "/pc icon.png",
+    src: "/images/icons/pc icon.png",
     href: "/shop/computers",
   },
   {
     id: 8,
     title: "Принтери",
-    src: "/printers icon.png",
+    src: "/images/icons/printers icon.png",
     href: "/shop/printers",
   },
   {
     id: 9,
     title: "Навушники",
-    src: "/ear buds icon.png",
+    src: "/images/icons/ear buds icon.png",
     href: "/shop/earbuds",
   },
   {
     id: 10,
     title: "Навушники",
-    src: "/headphone icon.png",
+    src: "/images/icons/headphone icon.png",
     href: "/shop/headphones",
   },
 ];
@@ -66,19 +66,19 @@ export const incentives = [
     name: "Безкоштовна доставка",
     description:
       "Наша доставка абсолютно безкоштовна і це дуже подобається нашим клієнтам.",
-    imageSrc: "/shipping icon.png",
+    imageSrc: "/images/icons/shipping icon.png",
   },
   {
     name: "Підтримка клієнтів 24/7",
     description:
       "Наша підтримка працює цілодобово, щоб відповісти на будь-яке ваше запитання.",
-    imageSrc: "/support icon.png",
+    imageSrc: "/images/icons/support icon.png",
   },
   {
     name: "Швидкий кошик для покупок",
     description:
       "Ми маємо надзвичайно швидкий досвід покупок, і вам це сподобається.",
-    imageSrc: "/fast shopping icon.png",
+    imageSrc: "/images/icons/fast shopping icon.png",
   },
 ];
 
