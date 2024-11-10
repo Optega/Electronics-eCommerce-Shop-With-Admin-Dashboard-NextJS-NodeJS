7f3f74fe1b1e7b94768862b9573a1206387be581
Optega <optega@gmail.com>
2024-11-10 15:19:43 +0200

    feat: add demo user data and optimize data insertion for categories and products

--- server/utills/insertDemoData.js
+++ server/utills/insertDemoData.js
@@ -2,6 +2,21 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const demoUsers = [
+  {
+    id: "1",
+    email: "admin@gmail.com",
+    password: "$2a$05$aJrxk.qJDHPiL97a7/Mi5exgxjpWGUCgcCOG6tJ0F3aP072YcHIzG", // password: password
+    role: "ADMIN",
+  },
+  {
+    id: "2",
+    email: "user@gmail.com",
+    password: "$2a$05$aJrxk.qJDHPiL97a7/Mi5exgxjpWGUCgcCOG6tJ0F3aP072YcHIzG", // password: password
+    role: "USER",
+  },
+];
+
 const demoProducts = [
   {
     id: "1",
@@ -14,6 +29,11 @@ const demoProducts = [
     manufacturer: "Samsung",
     categoryId: "3117a1b0-6369-491e-8b8b-9fdd5ad9912e",
     inStock: 0,
+    sku: "rs-101",
+    reviewsCount: 1,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "2",
@@ -26,6 +46,11 @@ const demoProducts = [
     manufacturer: "Canon",
     categoryId: "659a91b9-3ff6-47d5-9830-5e7ac905b961",
     inStock: 0,
+    sku: "rs-102",
+    reviewsCount: 2,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "3",
@@ -38,6 +63,11 @@ const demoProducts = [
     manufacturer: "ZunVolt",
     categoryId: "6c3b8591-b01e-4842-bce1-2f5585bf3a28",
     inStock: 1,
+    sku: "rs-103",
+    reviewsCount: 3,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "4",
@@ -50,6 +80,11 @@ const demoProducts = [
     manufacturer: "Samsung",
     categoryId: "d30b85e2-e544-4f48-8434-33fe0b591579",
     inStock: 1,
+    sku: "rs-104",
+    reviewsCount: 4,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "5",
@@ -62,6 +97,11 @@ const demoProducts = [
     manufacturer: "Samsung",
     categoryId: "ada699e5-e764-4da0-8d3e-18a8c8c5ed24",
     inStock: 1,
+    sku: "rs-105",
+    reviewsCount: 5,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "6",
@@ -74,6 +114,11 @@ const demoProducts = [
     manufacturer: "Samsung",
     categoryId: "1cb9439a-ea47-4a33-913b-e9bf935bcc0b",
     inStock: 1,
+    sku: "rs-106",
+    reviewsCount: 6,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "7",
@@ -86,6 +131,11 @@ const demoProducts = [
     manufacturer: "SOWO",
     categoryId: "7a241318-624f-48f7-9921-1818f6c20d85",
     inStock: 1,
+    sku: "rs-107",
+    reviewsCount: 5,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "8",
@@ -98,6 +148,11 @@ const demoProducts = [
     manufacturer: "Bosch",
     categoryId: "8d2a091c-4b90-4d60-b191-114b895f3e54",
     inStock: 1,
+    sku: "rs-108",
+    reviewsCount: 4,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "9",
@@ -110,6 +165,11 @@ const demoProducts = [
     manufacturer: "Sony",
     categoryId: "4c2cc9ec-7504-4b7c-8ecd-2379a854a423",
     inStock: 1,
+    sku: "rs-109",
+    reviewsCount: 3,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "10",
@@ -122,6 +182,11 @@ const demoProducts = [
     manufacturer: "Samsung",
     categoryId: "a6896b67-197c-4b2a-b5e2-93954474d8b4",
     inStock: 1,
+    sku: "rs-110",
+    reviewsCount: 2,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "11",
@@ -134,6 +199,11 @@ const demoProducts = [
     manufacturer: "HP",
     categoryId: "782e7829-806b-489f-8c3a-2689548d7153",
     inStock: 1,
+    sku: "rs-111",
+    reviewsCount: 1,
+    attributes: {
+      color: "black",
+    },
   },
   {
     id: "12",
@@ -146,6 +216,11 @@ const demoProducts = [
     manufacturer: "Gillete",
     categoryId: "313eee86-bc11-4dc1-8cb0-6b2c2a2a1ccb",
     inStock: 0,
+    sku: "rs-112",
+    reviewsCount: 0,
+    attributes: {
+      color: "black",
+    },
   },
 ];
 
@@ -228,19 +303,20 @@ const demoCategories = [
 ];
 
 async function insertDemoData() {
-  for (const category of demoCategories) {
-    await prisma.category.create({
-      data: category,
-    });
-  }
+  await prisma.category.createMany({
+    data: demoCategories,
+  });
   console.log("Demo categories inserted successfully!");
 
-  for (const product of demoProducts) {
-    await prisma.product.create({
-      data: product,
-    });
-  }
+  await prisma.product.createMany({
+    data: demoProducts,
+  });
   console.log("Demo products inserted successfully!");
+
+  await prisma.user.createMany({
+    data: demoUsers,
+  });
+  console.log("Demo users inserted successfully!");
 }
 
 insertDemoData()
